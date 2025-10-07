8ba66aa453aef27eea6f31a1c14f9b6e5a12c31c
ghostcat0x1 <ghostcat0x1@gmail.com>
2025-10-08 00:58:17 +0800

    Update Videodlt.js

--- Videodlt.js
+++ Videodlt.js
@@ -12,9 +12,9 @@ async function deleteVideo() {
     if (videos.data.length > 0) {
       const videoId = videos.data[0].uri.split("/").pop(); // Extract ID
       // Step 2: Delete
-      const deleteRes = await fetch(`https://api.vimeo.com/videos/${videoId}`, {
+      const deleteRes = await fetch(`https://api.vimeo.com/videos/${1124993996}`, {
         method: "DELETE",
-        headers: { Authorization: "jwt YOUR_TOKEN_HERE" },
+        headers: { Authorization: "jwt eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE3NTk4NTcxODAsInVzZXJfaWQiOjI0ODI1NDA0NywiYXBwX2lkIjo1ODQ3OSwic2NvcGVzIjoicHVibGljIGludGVyYWN0IHByaXZhdGUgdXBsb2FkIGNyZWF0ZSBlZGl0IGRlbGV0ZSBlbWFpbCBzdGF0cyB2aWRlb19maWxlcyIsInRlYW1fdXNlcl9pZCI6bnVsbCwianRpIjoiMDAzM2UxNWItMDYwYS00MGIyLTg2ZTYtZWJhZTcyY2RjYzgxIiwic2giOiJjOWJmYjczMmQzYjEyNGI2NGIwNWM3OTBjZDYxMjhmNjdhZjA3OTVmZDcyMzBlNGQ1NWZhZGU1MDllMjQ2ODU3In0.69ObgaEqgkvaq6yCGeCPiLILRUQPQbcx1hq-cSgzLGk" },
       });
       if (deleteRes.ok) {
         resultDiv.innerHTML += "<p>Video Deleted! High Impact Achieved.</p>";
@@ -29,3 +29,4 @@ async function deleteVideo() {
   }
 }
 // Button add: <button onclick="deleteVideo()">Delete My First Video!</button>
+
